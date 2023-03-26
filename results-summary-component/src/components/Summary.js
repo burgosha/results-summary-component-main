2ff93c89f6849674f399d3b9137218a890818438
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-03-26 16:37:13 -0300

    Agregado de estilos - Mobile first

--- results-summary-component/src/components/Summary.js
+++ results-summary-component/src/components/Summary.js
@@ -1,55 +1,61 @@
 import React from "react";
+import "../stylesheets/Summary.css";
 import ReactionSvg from "../images/icon-reaction.svg";
 function Summary() {
     return(
         <div className="main-container">
             <div className="result-container">
-                <p>Your Result</p>
-                <div>76 of 100</div>
-                <p>Great</p>
-                <p>Your performance exceed 65% of the people conducting the test here!</p>
+                <p className="result-title">Your Result</p>
+                <div className="result-score-container"><p className="result-score">76</p><p className="of-100">of 100</p></div>
+                <div className="result-notes">
+                    <p className="result-note">Great</p>
+                    <p className="result-description">Your performance exceed 65% of the people conducting the test here!</p>
+                </div>
             </div>
             <div className="summary-container">
                 <p className="summary-title">Summary</p>
-                {/* Score */}
-                <div className="score-tag reaction-tag">
-                    <span className="reaction-text">
-                        <img className="reaction-icon" src={ReactionSvg} alt="reaction-icon" />
-                        Reaction
-                    </span>
-                    <span className="reaction-score">
-                        <span className="reaction-score-num">80</span> / 100
-                    </span>
-                </div>
-                {/* Score */}
-                <div className="score-tag reaction-tag">
-                    <span className="reaction-text">
-                        <img className="reaction-icon" src={ReactionSvg} alt="reaction-icon" />
-                        Reaction
-                    </span>
-                    <span className="reaction-score">
-                        <span className="reaction-score-num">80</span> / 100
-                    </span>
-                </div>
-                {/* Score */}
-                <div className="score-tag reaction-tag">
-                    <span className="reaction-text">
-                        <img className="reaction-icon" src={ReactionSvg} alt="reaction-icon" />
-                        Reaction
-                    </span>
-                    <span className="reaction-score">
-                        <span className="reaction-score-num">80</span> / 100
-                    </span>
-                </div>
-                {/* Score */}
-                <div className="score-tag reaction-tag">
-                    <span className="reaction-text">
-                        <img className="reaction-icon" src={ReactionSvg} alt="reaction-icon" />
-                        Reaction
-                    </span>
-                    <span className="reaction-score">
-                        <span className="reaction-score-num">80</span> / 100
-                    </span>
+                {/* Scores */}
+                <div className="scores">
+                    {/* Score */}
+                    <div className="score-tag reaction-tag">
+                        <span className="reaction-text">
+                            <img className="reaction-icon" src={ReactionSvg} alt="reaction-icon" />
+                            Reaction
+                        </span>
+                        <span className="reaction-score">
+                            <span className="reaction-score-num">80</span> / 100
+                        </span>
+                    </div>
+                    {/* Score */}
+                    <div className="score-tag reaction-tag">
+                        <span className="reaction-text">
+                            <img className="reaction-icon" src={ReactionSvg} alt="reaction-icon" />
+                            Reaction
+                        </span>
+                        <span className="reaction-score">
+                            <span className="reaction-score-num">80</span> / 100
+                        </span>
+                    </div>
+                    {/* Score */}
+                    <div className="score-tag reaction-tag">
+                        <span className="reaction-text">
+                            <img className="reaction-icon" src={ReactionSvg} alt="reaction-icon" />
+                            Reaction
+                        </span>
+                        <span className="reaction-score">
+                            <span className="reaction-score-num">80</span> / 100
+                        </span>
+                    </div>
+                    {/* Score */}
+                    <div className="score-tag reaction-tag">
+                        <span className="reaction-text">
+                            <img className="reaction-icon" src={ReactionSvg} alt="reaction-icon" />
+                            Reaction
+                        </span>
+                        <span className="reaction-score">
+                            <span className="reaction-score-num">80</span> / 100
+                        </span>
+                    </div>    
                 </div>
                 {/* Continue Button */}
                 <button className="continue-button">Continue</button>
